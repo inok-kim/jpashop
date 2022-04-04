120e7a9e95b9b8e487ecf699b595a7d077091d75
iokim <iokim1220@gmail.com>
2022-04-04 14:11:13 +0900

    # 실전 예제 1 - 요구사항 분석과 기본 매핑

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        // 애플리케이션 로딩 시점에 딱 하나만 만들어야함, 애플리케이션 전체에서 공유
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // 트랜잭션 단위마다 엔티티 매니저 만들어줘야함, 쓰레드간에 공유X, 사용하고 버려야함
+        EntityManager em = emf.createEntityManager();
+
+        // JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member memberA = new Member();
+
+            System.out.println("======================");
+            em.persist(memberA);
+            System.out.println("memberA.getId() = " + memberA.getId());
+            System.out.println("======================");
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+
+}
