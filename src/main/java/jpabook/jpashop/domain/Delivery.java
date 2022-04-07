c46ba709b92957d4f094414121b714086f3543e3
iokim <iokim1220@gmail.com>
2022-04-07 15:36:26 +0900

    # 실전 예제 5 - 연관관계 관리 Order 부모 OrderItem, Delivery - CascadeType.ALL => 부모의 상태 전이(영속성 전이)

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 public class Delivery extends BaseEntity {
 
@@ -13,9 +15,7 @@ public class Delivery extends BaseEntity {
     private String zipcode;
     private DeliveryStatus status;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
-
-
 
 }
